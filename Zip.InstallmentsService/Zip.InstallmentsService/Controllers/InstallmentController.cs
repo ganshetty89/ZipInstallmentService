82ee3244896d1efa0e9bf05b31ba12139d6b214b
ganshetty89 <58112466+ganshetty89@users.noreply.github.com>
2022-10-16 09:34:42 +0530

    Pushing Initial Skeleton

---
+++ Zip.InstallmentsService/Zip.InstallmentsService/Controllers/InstallmentController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Zip.InstallmentsService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InstallmentController : ControllerBase
+    {
+    }
+}
