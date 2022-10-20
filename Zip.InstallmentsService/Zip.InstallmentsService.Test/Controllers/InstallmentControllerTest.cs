24b426b5c7476dcf69a09f13d713a3c337af2481
ganshetty89 <58112466+ganshetty89@users.noreply.github.com>
2022-10-20 10:29:29 +0530

    Implemented Unit Tests

---
+++ Zip.InstallmentsService/Zip.InstallmentsService.Test/Controllers/InstallmentControllerTest.cs
@@ -0,0 +1,40 @@
+﻿using FakeItEasy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zip.InstallmentsService.Controllers;
+using Zip.InstallmentsService.Model;
+using Zip.InstallmentsService.Service.Interface;
+
+namespace Zip.InstallmentsService.Test.Controllers
+{
+    [TestClass]
+    public class InstallmentControllerTest
+    {
+        private InstallmentController _installmentController;
+        private IPaymentPlanService _paymentPlanService;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _paymentPlanService = A.Fake<IPaymentPlanService>();
+            _installmentController = new InstallmentController(_paymentPlanService);
+        }
+
+        [TestMethod]
+        public async Task GetPaymentPlanAsync_Success()
+        {
+            var paymentPlanInput = A.Fake<PaymentPlanInput>();
+            var paymentPlan = A.Fake<PaymentPlan>();
+
+            A.CallTo(() => _paymentPlanService.CreatePaymentPlan(paymentPlanInput)).Returns(paymentPlan);
+
+            var result = await _installmentController.GetPaymentPlanAsync(paymentPlanInput);
+
+            Assert.AreEqual(paymentPlan, result);
+        }
+    }
+}
---
+++ Zip.InstallmentsService/Zip.InstallmentsService.Test/Service/PaymentPlanServiceTest.cs
@@ -0,0 +1,56 @@
+﻿using FakeItEasy;
+using Microsoft.Extensions.Logging;
+using Zip.InstallmentsService.Model;
+using Zip.InstallmentsService.Service;
+using Zip.InstallmentsService.Service.Interface;
+
+namespace Zip.InstallmentsService.Test.Service
+{
+    [TestClass]
+    public class PaymentPlanServiceTest
+    {
+        private ILogger<PaymentPlanService> _logger;
+        private IPaymentPlanService _paymentPlanService;
+
+        [TestInitialize]
+        public void TestInitialization()
+        {
+            _logger = A.Fake<ILogger<PaymentPlanService>>();
+            _paymentPlanService = new PaymentPlanService(_logger);
+        }
+
+        [TestMethod]
+        public async Task CreatePaymentPlan_Success()
+        {
+            //arrange
+            var paymentPlanInput = new PaymentPlanInput()
+            {
+                Frequency = 4,
+                NoOfInstallments = 4,
+                PurchaseAmount = 200
+            };
+
+            //act
+            var result = await _paymentPlanService.CreatePaymentPlan(paymentPlanInput);
+
+            //assert
+            Assert.AreEqual(result.PurchaseAmount, paymentPlanInput.PurchaseAmount);
+            Assert.AreEqual(result.Installments[0].Amount, Math.Round(paymentPlanInput.PurchaseAmount / paymentPlanInput.NoOfInstallments, 2));
+        }
+
+        [TestMethod]
+        public async Task CreatePaymentPlan_Exception()
+        {
+            //arrange
+            var paymentPlanInput = new PaymentPlanInput()
+            {
+                Frequency = 4,
+                NoOfInstallments = 0,
+                PurchaseAmount = 200
+            };
+
+            //act and assert
+            await Assert.ThrowsExceptionAsync<DivideByZeroException>(async () => await _paymentPlanService.CreatePaymentPlan(paymentPlanInput));
+        }
+    }
+}
